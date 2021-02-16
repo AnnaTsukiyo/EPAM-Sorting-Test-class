50461ffbf01fbf861bb9c10d47f8ff43ae75fee2
thejerome <thejerome@mail.ru>
2021-02-16 22:55:36 +0300

    Initial Commit

---
+++ src/main/java/com/epam/rd/autotasks/Sorting.java
@@ -0,0 +1,7 @@
+package com.epam.rd.autotasks;
+
+public class Sorting {
+    public void sort(int[] array){
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ src/main/java/com/epam/rd/autotasks/SortingTest.java
@@ -0,0 +1,35 @@
+package com.epam.rd.autotasks;
+
+import org.junit.Test;
+
+
+public class SortingTest {
+
+    Sorting sorting = new Sorting();
+
+    @Test
+    public void testNullCase(){
+
+    }
+
+    @Test
+    public void testEmptyCase(){
+
+    }
+
+    @Test
+    public void testSingleElementArrayCase() {
+
+    }
+
+    @Test
+    public void testSortedArraysCase() {
+
+    }
+
+    @Test
+    public void testOtherCases() {
+
+    }
+
+}
